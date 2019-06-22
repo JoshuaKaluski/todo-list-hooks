4186a8e6ede0947bede716fd84aea822229710c0
Joshua Kaluski <joshua@kaluski.com>
2019-06-22 15:11:17 -0400

    Added checkbox, delete, and edit to Todo items

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,8 @@
 import React, {useState} from 'react';
+import Todo from './Todo';
 
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 
 
@@ -13,9 +12,7 @@ function TodoList(props) {
       <List>
         {props.todos.map(todo => (
           <>
-            <ListItem>
-              <ListItemText>{todo.task}</ListItemText>
-            </ListItem>
+            <Todo key={todo.id} task={todo.task} completed={todo.completed}/>
             <Divider/>
           </>
         ))}
