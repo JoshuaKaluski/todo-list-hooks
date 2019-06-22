b7c0e14fb57f27a705a2294ae96ee14a3c24b38b
Joshua Kaluski <joshua@kaluski.com>
2019-06-22 14:23:19 -0400

    Added form

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
@@ -15,6 +16,10 @@ function TodoApp() {
   ];
   const [todos, setTodos] = useState(initialTodos);
 
+  const addTodo = newTodoText => {
+    setTodos([...todos, {id: 4, task: newTodoText, completed: false}])
+  };
+
   return (
     <Paper style={{
       padding: 0,
@@ -27,6 +32,7 @@ function TodoApp() {
           <Typography color="inherit">ToDos with Hooks</Typography>
         </Toolbar>
       </AppBar>
+      <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} />
     </Paper>
   )
